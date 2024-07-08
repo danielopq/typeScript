1ea3957387d6f06be89afdd9a358f6ae380b8107
Daniel Martinez Duque <danielopq@gmail.com>
2024-07-08 13:20:15 +0100

    types

---
+++ 01 - Intro/objetos/type.js
@@ -0,0 +1,36 @@
+"use strict";
+(() => {
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['super velocidad', 'viajar en el tiempo'],
+        getName() {
+            return this.name;
+        }
+    };
+    let flash2 = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['super velocidad', 'viajar en el tiempo'],
+        getName() {
+            return this.name;
+        }
+    };
+    let superMan = {
+        name: 'Clark Kent',
+        age: 42,
+        powers: ['super velocidad', 'rayos x'],
+        getName() {
+            return this.name;
+        }
+    };
+    let spiderman = {
+        name: 'Peter Parker',
+        age: 21,
+        powers: ['sentido arácnido', 'crear telarañas'],
+        getName() {
+            return this.name;
+        }
+    };
+    //
+})();
---
+++ 01 - Intro/objetos/type.ts
@@ -0,0 +1,50 @@
+(()=>{
+
+    let flash: {name:string,age?:number,powers:string[],getName:()=>string} = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['super velocidad','viajar en el tiempo'],
+        getName(){
+            return this.name;
+        }
+    }
+
+    //el ejemplo de arriba lo podemos resolver de la siguiente forma:
+
+    type Hero = {
+        name:string;
+        age?:number;
+        powers:string[];
+        getName:()=>string;
+    }
+
+    let flash2: Hero = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['super velocidad','viajar en el tiempo'],
+        getName(){
+            return this.name;
+        }
+    }
+
+    let superMan: Hero = {
+        name: 'Clark Kent',
+        age: 42,
+        powers: ['super velocidad','rayos x'],
+        getName(){
+            return this.name;
+        }
+    }
+
+    let spiderman: Hero = {
+        name: 'Peter Parker',
+        age: 21,
+        powers: ['sentido arácnido','crear telarañas'],
+        getName(){
+            return this.name;
+        }
+    }
+
+    //
+
+})()
