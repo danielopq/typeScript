20504f08029101e04ead581ff75fdcf841c78b40
Daniel Martinez Duque <danielopq@gmail.com>
2024-07-09 13:46:57 +0100

    class methods

--- classes/basica.ts
+++ classes/basica.ts
@@ -1,8 +1,10 @@
 (()=>{
+
+    //forma larga de declarar una clase
     class Avenger {
         private name : string;
         private team: string;
-        private realName?: string;
+        public realName?: string;
         static age: number = 35;
 
         constructor(name:string,team:string,realname?:string){
@@ -13,7 +15,25 @@
 
     }
 
+    
+    //forma corta de declarar una clase
+
+    class Avenger2 {
+        static age: number = 35;
+
+        constructor(
+            private name:string,
+            private team:string,
+            public realname?:string
+        ){}
+
+        public getName(){
+            return this.name;
+        }
+    }
 
     const antman: Avenger = new Avenger('Antmam','Team A');
-    console.log(antman);
+    console.log('antman:', antman);
+    const superman: Avenger2 = new Avenger2('superman','Team B');
+    console.log('superman:', superman);
 })()
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
 (() => {
+    //forma larga de declarar una clase
     class Avenger {
         constructor(name, team, realname) {
             this.name = name;
@@ -8,7 +9,22 @@
         }
     }
     Avenger.age = 35;
+    //forma corta de declarar una clase
+    class Avenger2 {
+        constructor(name, team, realname) {
+            this.name = name;
+            this.team = team;
+            this.realname = realname;
+        }
+        getName() {
+            return this.name;
+        }
+    }
+    Avenger2.age = 35;
     const antman = new Avenger('Antmam', 'Team A');
-    console.log(antman);
+    console.log('antman:', antman);
+    const superman = new Avenger2('superman', 'Team B');
+    console.log('superman:', superman);
+    console.log(superman.getName())
 })();
 //# sourceMappingURL=main.js.map
